89cf45af2bc3dee6fd5a8bd70fabc5560e07209f
unknown <SerDImIgor@gmail.com>
2022-03-05 18:35:48 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+# https://packaging.python.org/en/latest/guides/installing-using-pip-and-virtual-environments/
+# Set-ExecutionPolicy Unrestricted
+# Set-ExecutionPolicy Restricted
+
+# https://www.youtube.com/watch?v=x8hVoalU0MA
+# https://www.youtube.com/watch?v=x8hVoalU0MA
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
