8fb36875b5fba1366e8c8f8411dc8be1e2db68b1
unknown <SerDImIgor@gmail.com>
2022-03-08 11:13:20 +0300

    add exception

--- hello.py
+++ hello.py
@@ -59,7 +59,7 @@ def handle_message(event):
             original_content_url = 'https://heroku-flaskn.herokuapp.com/images/cat.jpg',
             preview_image_url = 'https://heroku-flaskn.herokuapp.com/images/catp.jpg')
             )
-    except Exception as e 
+    except Exception as e:
         app.logger.error('This is an ERROR log record.'+ str(e) )
 
 
